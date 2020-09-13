7fb72b29fe24e82facf22302fe81386e3382dd45
Artur Drzyzga <artek.drzyzga@gmail.com>
2020-09-13 08:12:13 +0100

    pictures resizing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_one_attached :picture
+
+  def thumb
+   return self.picture.variant(resize: '640x480!')
+  end
+
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,8 +31,8 @@ Rails.application.configure do
 
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  #config.active_storage.service = :local
-  config.active_storage.service = :google_dev
+  config.active_storage.service = :local
+  #config.active_storage.service = :google_dev
 
 
 
