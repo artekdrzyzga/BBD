6061d5e1c0c76ce3921bc13c545cde141c52e68e
Artur Drzyzga <artek.drzyzga@gmail.com>
2020-10-30 16:05:39 +0000

    email port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,16 +69,15 @@ Rails.application.configure do
 
 
 
-
-config.action_mailer.delivery_method = :smtp
-# SMTP settings for gmail
-config.action_mailer.smtp_settings = {
- :address              => "smtp.hosts.co.uk",
- :port                 => 587,
- :user_name            => ENV['gmail_username'],
- :password             => ENV['gmail_password'],
- :authentication       => "login",
-:enable_starttls_auto => true
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+   :authentication       => "plain",
+  :enable_starttls_auto => true
 }
   config.action_mailer.default_url_options = { :host => "bbd-shop.herokuapp.com" }
 
