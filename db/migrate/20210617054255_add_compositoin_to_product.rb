9c079f9a7a9f775de421fa7c9cfafda512804a03
Artur Drzyzga <artek.drzyzga@gmail.com>
2021-06-17 22:13:40 +0200

    add quantity

--- db/migrate/20210617054255_add_compositoin_to_product.rb
+++ db/migrate/20210617054255_add_compositoin_to_product.rb
@@ -1,5 +1,6 @@
 class AddCompositoinToProduct < ActiveRecord::Migration[5.2]
   def change
+    add_column :products, :quantity, :integer
     add_column :products, :composition, :string
   end
 end
