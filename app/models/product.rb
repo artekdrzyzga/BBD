999651266251326ce308f9d28897f8be196d9a3c
Artur Drzyzga <artek.drzyzga@gmail.com>
2021-07-12 20:46:21 +0200

    update picture size

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,11 +3,12 @@ class Product < ApplicationRecord
   has_one_attached :picture
 
   validates :name, presence: true
-  validates :code, presence: true
+  validates :code, presence: true, uniqueness: true
   validates :price, presence: true
 
+
   def thumb
-   return self.picture.variant(resize: '640x480!')
+   return self.picture.variant(resize: '416x256!')
   end
 
 
